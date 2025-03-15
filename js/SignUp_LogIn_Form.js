97f1c382c0edbe87d8fd16e4f5bdf01d1e351c02
MMSSB <56227348+MMSSB@users.noreply.github.com>
2025-03-15 11:09:44 +0200

    Add files via upload

--- js/SignUp_LogIn_Form.js
+++ js/SignUp_LogIn_Form.js
@@ -13,25 +13,25 @@ loginBtn.addEventListener('click', () => {
     body.classList.remove('register-gradient'); // Remove register gradient class
 });
 
-document.addEventListener('DOMContentLoaded', () => {
-    const loginForm = document.getElementById('loginForm');
-    const errorMessage = document.getElementById('error-message');
+// document.addEventListener('DOMContentLoaded', () => {
+//     const loginForm = document.getElementById('loginForm');
+//     const errorMessage = document.getElementById('error-message');
 
-    // Hard-coded username and password
-    const correctUsername = 'MMS';
-    const correctPassword = '0000';
+//     // Hard-coded username and password
+//     const correctUsername = 'MMS';
+//     const correctPassword = '0902';
 
-    loginForm.addEventListener('submit', (event) => {
-        event.preventDefault();
+//     loginForm.addEventListener('submit', (event) => {
+//         event.preventDefault();
 
-        const username = document.getElementById('username').value;
-        const password = document.getElementById('password').value;
+//         const username = document.getElementById('username').value;
+//         const password = document.getElementById('password').value;
 
-        if (username === correctUsername && password === correctPassword) {
-            localStorage.setItem('loggedIn', 'true');
-            window.location.href = '../loading1.html'; // Redirect to the loading page
-        } else {
-            errorMessage.textContent = 'Invalid username or password.';
-        }
-    });
-});
+//         if (username === correctUsername && password === correctPassword) {
+//             localStorage.setItem('loggedIn', 'true');
+//             window.location.href = '../loading1.html'; // Redirect to the loading page
+//         } else {
+//             errorMessage.textContent = 'Invalid username or password.';
+//         }
+//     });
+// });
