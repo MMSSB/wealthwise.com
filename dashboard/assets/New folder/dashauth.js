b4617fb3c1909ab0d3d1edb4dd566d766d660533
MMSSB <56227348+MMSSB@users.noreply.github.com>
2025-09-07 15:52:55 +0300

    Update dashauth.js

--- dashboard/assets/New folder/dashauth.js
+++ dashboard/assets/New folder/dashauth.js
@@ -3,13 +3,13 @@ import { getAuth, onAuthStateChanged, signOut } from "https://www.gstatic.com/fi
 import { getFirestore, getDoc, doc } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBLCMK2Tm9sk2EBnhqoK9j_y8jiATd-QDA",
-    authDomain: "wealth-wise-3c329.firebaseapp.com",
-    projectId: "wealth-wise-3c329",
-    storageBucket: "wealth-wise-3c329.firebasestorage.app",
-    messagingSenderId: "546585897291",
-    appId: "1:546585897291:web:111d6c2d3170d93f59e0ce",
-    measurementId: "G-PGTS22BHQP"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 // Initialize Firebase
@@ -118,4 +118,5 @@ logoutButtonpro.addEventListener('click', () => {
 
 
 // export { db, storage };
+
 
